8d00a3e8ece24f2ed020ef0c33b5d262f0b24fc5
mustafamercan775-ship-it <mustafamercan775@gmail.com>
2026-03-13 21:46:37 +0300

    Initial commit

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'caripro-v9';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Drive API isteklerini cache'leme
+  if (e.request.url.includes('drive.google.com') || e.request.url.includes('googleapis.com')) {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put('drive-data', clone));
+        return res;
+      }).catch(() => caches.match('drive-data'))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
